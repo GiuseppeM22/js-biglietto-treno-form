cf66b3c844c5a2bd749e0c4c1441eee326d280ec
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-14 16:28:53 +0200

    feat: inizio js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-let
+let btn = document.getElementById("done");
+btn.addEventListener("click",
+    
+    function () {
+        let passengerName = document.getElementById("passengerName").value;
+        console.log("Generazione nome del passeggero:", passengerName);
+        document.getElementById("passenger").innerText = passengerName
+
+        
+    })
