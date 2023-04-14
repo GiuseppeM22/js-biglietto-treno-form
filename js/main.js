12517f99f39a5de462ca02ec5dd8ec52b96339ed
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-04-14 17:28:20 +0200

    feat: add all js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,37 @@ btn.addEventListener("click",
     function () {
         let passengerName = document.getElementById("passengerName").value;
         console.log("Generazione nome del passeggero:", passengerName);
-        document.getElementById("passenger").innerText = passengerName
 
+        let chilometri = document.getElementById("chilometri").value;
+
+        let age = document.getElementById("age").value;
+
+        let prezzoPerKm = 0.21
+        let prezzoTotale = chilometri * prezzoPerKm
+        let tipoSconto;
         
+        if (age  == "minorenne"){
+         prezzoTotale = prezzoTotale - ( prezzoTotale * 0.2 )
+         tipoSconto = "Sconto Minori"
+        }
+         else if (age == "over65"){
+         prezzoTotale = prezzoTotale - ( prezzoTotale * 0.4 )
+         tipoSconto = "Sconto Senior"
+
+         }
+         else {
+            prezzoTotale = prezzoTotale
+            tipoSconto = "Nessuno Sconto"
+
+         }
+
+
+         prezzoTotale = prezzoTotale.toFixed(2)
+
+         document.getElementById("passenger").innerText = passengerName
+         document.getElementById("sale").innerText = tipoSconto
+         document.getElementById("price").innerText = prezzoTotale + " € "
+
+         
+
     })
